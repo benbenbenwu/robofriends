3acff0e372f477820ed6a1f36fff0aae4b7caa6d
ben <ben@example.com>
2022-12-07 17:24:15 +0800

    first vision

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from './SearchBox'
+import { robots } from './robots'
 
-function App() {
-  return (
-    <div >
-      Hello world
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchFiled: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchFiled: event.target.value })
+
+  }
+
+  render() {
+    const filterRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchFiled.toLowerCase()))
+
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filterRobots} />
+      </div>
+    )
+  };
 }
 
 export default App;
